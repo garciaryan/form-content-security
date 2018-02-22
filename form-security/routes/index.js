981ad56d4667bcbc19bef370eb3cee44f1aeaaa9
Ryan Garcia <garciaryan007@yahoo.com>
2018-02-21 20:44:02 -0800

    content-security-policy for form action

--- form-security/routes/index.js
+++ form-security/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+//secure homepage
+router.get('/csp', function(req, res, next) {
+  res.header('Content-Security-Policy', `form-action 'self';`);
+  res.render('index');
 });
 
 module.exports = router;
